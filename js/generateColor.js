d3ea2aa8716f33851b0355a65937c95cf13024e0
Jackson Henry <jacksonhenry3@gmail.com>
2016-08-28 18:16:24 -0700

    working and more modular

---
+++ js/generateColor.js
@@ -0,0 +1,19 @@
+function makeColorGradient(frequency1, frequency2, frequency3,
+                             phase1, phase2, phase3,
+                             center, width, len)
+  {
+  	colors = []
+    if (center == undefined)   center = 128;
+    if (width == undefined)    width = 127;
+    if (len == undefined)      len = 50;
+
+    for (var i = 0; i < len; ++i)
+    {
+       var red = Math.floor(Math.sin(frequency1*i + phase1) * width + center);
+       var grn = Math.floor(Math.sin(frequency2*i + phase2) * width + center);
+       var blu = Math.floor(Math.sin(frequency3*i + phase3) * width + center);
+       colors.push('rgba('+red.toString()+','+grn.toString()+','+blu.toString()+',.8)')
+    }
+    return(colors)
+  }
+var colors =  makeColorGradient(.3,.3,.3,0,2,4);
